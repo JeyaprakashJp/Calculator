8c48754915dc4a9daf55ef767c4586d0522716e3
Jeyaprakash J <87711232+JeyaprakashJp@users.noreply.github.com>
2023-07-22 15:55:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function by_ID(){
+   
+   
+
+    data1 = document.getElementById("num1").value;
+    data2 = document.getElementById("num2").value;
+    d1=parseInt(data1);
+    d2=parseInt(data2);
+    document.getElementById("para").innerHTML=d1+d2;
+   
+
+   
+}
+function by_SUB(){
+   
+   
+
+    data1 = document.getElementById("num1").value;
+    data2 = document.getElementById("num2").value;
+    d1=parseInt(data1);
+    d2=parseInt(data2);
+    document.getElementById("para").innerHTML=d1-d2;
+   
+
+   
+}
+function by_MUL(){
+   
+   
+
+    data1 = document.getElementById("num1").value;
+    data2 = document.getElementById("num2").value;
+    d1=parseInt(data1);
+    d2=parseInt(data2);
+    document.getElementById("para").innerHTML=d1*d2;
+   
+
+   
+}
+function by_DIV(){
+   
+   
+
+    data1 = document.getElementById("num1").value;
+    data2 = document.getElementById("num2").value;
+    d1=parseInt(data1);
+    d2=parseInt(data2);
+    document.getElementById("para").innerHTML=d1/d2;
+   
+
+   
+}
